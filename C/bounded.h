d3baff3578e361032d8732e2ba41d8459cc9d34e
Byron Hambly <byron@hambly.dev>
2024-01-05 15:24:53 +0200

    build(windows): specify overhead_value as #define
    Specifies the overhead_value via a #define, so that MSVC can use the
    value as an const initializer.

--- C/bounded.h
+++ C/bounded.h
@@ -8,11 +8,10 @@ typedef uint_least32_t ubounded;
 #define UBOUNDED_MAX UINT32_MAX
 
 /*
-Windows.h includes minwindef.h which defines a "max" macro that causes the
-following function definition to fail compilation with "error: C2059" on MSVC.
-
-https://github.com/tpn/winsdk-10/blob/9b69fd26ac0c7d0b83d378dba01080e93349c2ed/Include/10.0.10240.0/shared/minwindef.h#L193
-*/
+ * Windows.h includes minwindef.h which defines a "max" macro that causes the
+ * following function definition to fail compilation with "error: C2059" on MSVC.
+ * https://github.com/tpn/winsdk-10/blob/9b69fd26ac0c7d0b83d378dba01080e93349c2ed/Include/10.0.10240.0/shared/minwindef.h#L193
+ */
 #undef max
 
 static inline ubounded max(uint_least32_t x, uint_least32_t y) {
@@ -38,5 +37,11 @@ static inline ubounded pad(bool right, ubounded a, ubounded b) {
   return max(a, b) - (right ? b : a);
 }
 
-static const ubounded overhead = 100 /* milli weight units */;
+/*
+ * Specified as a #define to fix "error C2099: initializer is not a constant"
+ * when using this value as an initializer on MSVC.
+ */
+#define overhead_value 100; /* milli weight units */
+
+static const ubounded overhead = overhead_value;
 #endif
